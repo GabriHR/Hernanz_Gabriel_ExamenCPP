cdbbbdc5261baf01ddfd184ee5aa04a9df6f8b4c
GabriHR <ghernrod@myuax.com>
2023-11-09 11:41:39 +0100

    Función suma

--- 1_OperacionesBásicas.cpp
+++ 1_OperacionesBásicas.cpp
@@ -5,6 +5,8 @@ int main(){
     int a,b;
     cout<<"Inserte el valor de 'a': "; cin>>a;
     cout<<"Inserte el valor de 'b': "; cin>>b;
+    int suma=a+b;
+    cout<<"La suma de los valores dados es: "; cin>>suma;
 
     return 0;
 }
