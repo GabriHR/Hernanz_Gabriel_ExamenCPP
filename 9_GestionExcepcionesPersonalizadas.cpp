8c24c0b3e31196783d8092914a920ce5aa8f7233
GabriHR <ghernrod@myuax.com>
2023-11-09 13:42:14 +0100

    Definición clase estudiante.

--- 9_GestionExcepcionesPersonalizadas.cpp
+++ 9_GestionExcepcionesPersonalizadas.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
+using namespace std;
+class MateriaYaRegistradaException {
+public:
+    MateriaYaRegistradaException(const string& materia) : materia(materia) {}
+
+    string getMateria() const {
+        return materia;
+    }
+
+private:
+    string materia;
+};
