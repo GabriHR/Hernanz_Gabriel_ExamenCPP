a6cd7265a5e280bdd3362f9785694e8a921878e1
GabriHR <ghernrod@myuax.com>
2023-11-09 13:22:35 +0100

    Declaracion Vaiables en la clase RegistroAsistencia

--- 7_ManejoAusencias.cpp
+++ 7_ManejoAusencias.cpp
@@ -1,4 +1,15 @@
-#include "iostream"
-#include "string"
+#include <iostream>
+#include <string>
 using namespace std;
 
+class RegistroAsistencia{
+public:
+    RegistroAsistencia(string fecha, string estado);
+    void mostrar_asistencia() {
+        cout << "Fecha de la clase: " << fecha << ", Estado de asistencia: " << estado << endl;
+    }
+
+private:
+    string fecha;
+    string estado;
+};
