11da1e353b5bf9ce8318db797cccd633b11291dd
GabriHR <ghernrod@myuax.com>
2023-11-09 13:13:19 +0100

    Declaracion de variables.

--- 6_PromedioEstudiante.cpp
+++ 6_PromedioEstudiante.cpp
@@ -1,2 +1,13 @@
 #include "iostream"
 #include "vector"
+
+using namespace std;
+int main(){
+    vector<int> calificaciones = {8, 9, 7, 9, 8};
+
+    cout << "Mis calificaciones:";
+    for (int calificacion : calificaciones) {
+        cout << " " << calificacion;
+    }
+
+}
