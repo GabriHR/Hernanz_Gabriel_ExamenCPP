c61bb7462cf24e5b130557d0d7b5875893d02dcb
GabriHR <ghernrod@myuax.com>
2023-11-09 13:39:45 +0100

    Retoque de código y correción de errores.

--- 7_ManejoAusencias.cpp
+++ 7_ManejoAusencias.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 class RegistroAsistencia{
 public:
-    RegistroAsistencia(string fecha, string estado);
+    RegistroAsistencia(string fecha,string estado) {
+
+    }
+
     void mostrar_asistencia() {
         cout << "Fecha de la clase: " << fecha << ", Estado de asistencia: " << estado << endl;
     }
@@ -13,3 +16,15 @@ private:
     string fecha;
     string estado;
 };
+
+int main() {
+    RegistroAsistencia asistencia1("2023-11-07", "Tardanza");
+    RegistroAsistencia asistencia2("2023-11-08", "Falta");
+    RegistroAsistencia asistencia3("2023-11-09", "Asistio");
+
+    asistencia1.mostrar_asistencia();
+    asistencia2.mostrar_asistencia();
+    asistencia3.mostrar_asistencia();
+
+    return 0;
+}
