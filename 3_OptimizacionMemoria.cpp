3e960935d4048effed4b1f178882750f06b1e24a
GabriHR <ghernrod@myuax.com>
2023-11-09 12:06:57 +0100

    Declaración de los valores iniciales.

--- 3_OptimizacionMemoria.cpp
+++ 3_OptimizacionMemoria.cpp
@@ -6,3 +6,10 @@ void intercambiar(int* a, int*b){
     *a=*b;
     *b=temp;
 }
+
+int main(){
+    int n1=4;
+    int n2=44;
+    cout<<"Los valores iniciales son: \nnum1: "<<n1<<", \n2: "<<n2<<endl;
+
+}
