3aa2d6c715fd10f05a125f15dc98f4cb83f2a2c9
GabriHR <ghernrod@myuax.com>
2023-11-09 12:48:33 +0100

    Utilizacion de funciones temporales para agregrar materias a la clase estudiante y mostrarselas al usuario.

--- 5_RegistroMaterias.cpp
+++ 5_RegistroMaterias.cpp
@@ -6,6 +6,11 @@ using namespace std;
 class Estudiante{
 public:
     vector<string> materias;
+    void agregrar_materia(){
+        string materia;
+        cout<<"Ingrese las materias que tiene: "; cin>>materia;
+        materias.push_back(materia);
+    }
 
 
 };
