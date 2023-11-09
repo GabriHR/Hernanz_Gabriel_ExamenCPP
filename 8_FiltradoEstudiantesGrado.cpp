58e548ba432f678ebe34b6329e006913b6ee91b3
GabriHR <ghernrod@myuax.com>
2023-11-09 13:33:59 +0100

    Modificacion de la feed para que el usuario vea los grados de los diferentes estudiantes.

--- 8_FiltradoEstudiantesGrado.cpp
+++ 8_FiltradoEstudiantesGrado.cpp
@@ -25,6 +25,22 @@ int main() {
     estudiantes.push_back(Estudiante("Ruben", 10));
     estudiantes.push_back(Estudiante("Jose Luis", 8));
     estudiantes.push_back(Estudiante("Adriana", 9));
+    cout << "Grados disponibles:\n";
+    for (const Estudiante& estudiante : estudiantes) {
+        cout << estudiante.obtenerNombre() << " - Grado " << estudiante.obtenerGrado() << endl;
+    }
+
+    int gradoBuscado;
+    cout << "Elija un grado para filtrar: ";
+    cin >> gradoBuscado;
+
+    cout << "Estudiantes del grado " << gradoBuscado << ":\n";
+    for (const Estudiante& estudiante : estudiantes) {
+        if (estudiante.obtenerGrado() == gradoBuscado) {
+            cout << estudiante.obtenerNombre() << endl;
+        }
+    }
+    return 0;
 
 
 }
