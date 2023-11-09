4fb6e3cce236daac4fcd85522f4b498cd07861f6
GabriHR <ghernrod@myuax.com>
2023-11-09 12:51:11 +0100

    Determinar mas correctamente las materias y determinar la cantidad de caracteres a a poder añadir por el usuario.

--- 5_RegistroMaterias.cpp
+++ 5_RegistroMaterias.cpp
@@ -11,7 +11,12 @@ public:
         cout<<"Ingrese las materias que tiene: "; cin>>materia;
         materias.push_back(materia);
     }
-
+    void mostrar_materias(){
+        cout<<"Las materias que usted tiene son:\n"<<endl;
+        for (int i=0;i<materias.size();i++){
+            cout<<materias[i]<<endl;
+        }
+    }
 
 };
 
