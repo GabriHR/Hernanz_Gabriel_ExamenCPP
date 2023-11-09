afd6b744db08534274b05a98015c7f5a387d7446
GabriHR <ghernrod@myuax.com>
2023-11-09 11:39:27 +0100

    Declaración de variables de la función.

--- 1_OperacionesBásicas.cpp
+++ 1_OperacionesBásicas.cpp
@@ -1,2 +1,10 @@
 #include<iostream>
 using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Inserte el valor de 'a': "; cin>>a;
+    cout<<"Inserte el valor de 'b': "; cin>>b;
+
+    return 0;
+}
